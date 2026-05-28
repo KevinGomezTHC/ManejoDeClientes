44094e20cc527a8730afdb4467e8ebaa24f4bf06
KevinGomezTHC <admin4@thehospitalitycenter.net>
2026-05-28 11:49:39 -0500

    implementacion

--- crm/src/App.jsx
+++ crm/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -13,7 +13,7 @@ function App() {
 
   return (
 
-    <BrowserRouter>
+    <HashRouter>
 
       <Routes>
 
